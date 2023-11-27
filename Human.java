c7a95a481b711dbeae1b80f93ea20ae68326f344
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-11-26 21:07:33 -0600

    Update Human.java

--- Human.java
+++ Human.java
@@ -2,7 +2,7 @@ public class Human
 {
     public static int MaxAge = 72;
     public static int AgeAsApprentice = 18;
-    public static int YearToBeDisciple = 3;
+    public static int YearToBeDisciple = 1;
     public static int AgeOfGivingBirth = 30;
-    public static int CapOfApprentice = 2;
+    public static int CapOfApprentice = 1;
 }
